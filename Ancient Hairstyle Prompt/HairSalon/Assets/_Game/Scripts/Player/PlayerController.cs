0ece8c69fc6da421da5a3e898902a623f182431e
tonyli99 <tony@pixelcrushers.com>
2015-10-10 16:34:11 -0400

    Script updates

--- Ancient Hairstyle Prompt/HairSalon/Assets/_Game/Scripts/Player/PlayerController.cs
+++ Ancient Hairstyle Prompt/HairSalon/Assets/_Game/Scripts/Player/PlayerController.cs
@@ -5,17 +5,66 @@ public class PlayerController : MonoBehaviour {
 
     public string horizontalAxis = "Horizontal";
     public string verticalAxis = "Vertical";
+    public string actionButton = "Action1";
+    public float speed = 5;
+    public GameObject itemPosition;
 
     private CharacterController characterController;
+    private GameObject item = null;
+    private Table currentTable = null;
 
-	// Use this for initialization
-	void Start () {
+    void Start () {
         characterController = GetComponent<CharacterController>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        var motion = new Vector3(Input.GetAxis(horizontalAxis), 0, Input.GetAxis(verticalAxis));
-        characterController.Move(motion);	
+        var motion = new Vector3(Input.GetAxis(horizontalAxis) * Time.deltaTime * speed, 0, Input.GetAxis(verticalAxis) * Time.deltaTime * speed);
+        characterController.Move(motion);
+        if (Input.GetButtonDown(actionButton))
+        {
+            if (currentTable != null)
+            {
+                UseTable();
+            }
+        }
 	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        currentTable = other.GetComponent<Table>();
+        if (currentTable != null) Debug.Log("Entered table", currentTable);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (currentTable != null) Debug.Log("Left table", currentTable);
+        currentTable = null;
+    }
+
+    private void UseTable()
+    {
+        Debug.Log("Use table", currentTable);
+        if (currentTable.IsEmpty())
+        {
+            Debug.Log("Table is empty");
+            if (item != null)
+            {
+                Debug.Log("Leaving item");
+                currentTable.LeaveItem(item);
+                item = null;
+            }
+        }
+        else
+        {
+            Debug.Log("Table has something");
+            if (item == null)
+            {
+                Debug.Log("Taking item");
+                item = currentTable.TakeItem();
+                item.transform.SetParent(itemPosition.transform);
+                item.transform.localPosition = Vector3.zero;
+            }
+        }
+    }
+
 }
---
+++ Ancient Hairstyle Prompt/HairSalon/Assets/_Game/Scripts/Table/Table.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Table : MonoBehaviour
+{
+
+    public GameObject itemPosition;
+    public GameObject item = null;
+
+    public bool IsEmpty()
+    {
+        return (item == null);
+    }
+
+    public GameObject TakeItem()
+    {
+        var itemToReturn = item;
+        item = null;
+        //itemToReturn.SetActive(false);
+        return itemToReturn;
+    }
+
+    public void LeaveItem(GameObject itemToLeave)
+    {
+        if (IsEmpty() && (itemToLeave != null))
+        {
+            //itemToLeave.SetActive(true);
+            itemToLeave.transform.SetParent(itemPosition.transform);
+            itemToLeave.transform.localPosition = Vector3.zero;
+            item = itemToLeave;
+        }
+    }
+
+}
