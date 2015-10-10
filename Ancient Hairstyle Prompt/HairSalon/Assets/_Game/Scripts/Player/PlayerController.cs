22d22c0cf37f461b8f443fed78a924f21de98005
tonyli99 <tony@pixelcrushers.com>
2015-10-10 15:17:13 -0400

    Starter project

---
+++ Ancient Hairstyle Prompt/HairSalon/Assets/_Game/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+
+    private CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var motion = new Vector3(Input.GetAxis(horizontalAxis), 0, Input.GetAxis(verticalAxis));
+        characterController.Move(motion);	
+	}
+}
